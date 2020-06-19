de6ff4be3d6a1eebdec56e23426162292decc898
tegshee <tegshbayar@gmail.com>
2020-06-19 16:47:35 -0700

    Complete API GET request

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const controllers = require('./controllers.js');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`The app listening at http://localhost:${port}`));
 
 app.get('/api/rooms/:id', (req, res) => {
   controllers.get(req, res);
