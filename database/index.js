93a6a3fe6f632a53192b78c2d4ae55d0b8fe528c
tegshee <tegshbayar@gmail.com>
2020-06-19 13:36:44 -0700

    Refactor according to Eslint and add isSuperhost column into database

--- database/index.js
+++ database/index.js
@@ -1,10 +1,11 @@
-const config = require('../config.js')
-const mysql      = require('mysql');
+const mysql = require('mysql');
+const config = require('../config.js');
+
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : config.USERNAME_mysql_server,
-  password : config.PASSWORD_mysql_server,
-  database : 'airbnbClone'
+  host: 'localhost',
+  user: config.USERNAME_mysql_server,
+  password: config.PASSWORD_mysql_server,
+  database: 'airbnbClone',
 });
 
 connection.connect();
--- database/seed.js
+++ database/seed.js
@@ -16,50 +16,51 @@ const generateRoomsData = () => {
       location_country: faker.address.country(),
       average_review_point: (Math.random() * (5 - 1) + 1).toFixed(2),
       number_of_reviews: Math.floor(Math.random() * (250 - 1) + 1),
+      is_superhost: faker.random.boolean(),
     };
     roomsData.push(oneRoom);
   }
 };
 
 const seedRooms = (data) => {
-  const query = 'INSERT INTO rooms (room_name, location_city, location_country, average_review_point, number_of_reviews) VALUES(?, ?, ?, ?, ?)';
+  const query = 'INSERT INTO rooms (room_name, location_city, location_country, average_review_point, number_of_reviews, is_superhost) VALUES(?, ?, ?, ?, ?, ?)';
 
   for (let i = 0; i < data.length; i += 1) {
-    db.query(query, [data[i].room_name, data[i].location_city, data[i].location_country, data[i].average_review_point, data[i].number_of_reviews], (err, result, field) => {
+    db.query(query, [data[i].room_name, data[i].location_city, data[i].location_country, data[i].average_review_point, data[i].number_of_reviews, data[i].is_superhost], (err, result, field) => {
       if (err) {
         console.log(err);
       }
-    })
+    });
   }
 };
 
 const generateImagesData = () => {
 
   // Room_id starts from 1
-  for (let i = 1; i <= ITEM_NUMBER; i++) {
+  for (let i = 1; i <= ITEM_NUMBER; i += 1) {
     // One room has 5-15 images. Number of image is randomly generated
-    let randomNumberOfImage = Math.floor(Math.random()*(15 - 5) + 5);
+    const randomNumberOfImage = Math.floor(Math.random() * (15 - 5) + 5);
 
     // randomImageUrl will generate different image everytime
-    for (j = 0; j < randomNumberOfImage; j++) {
-      let oneImage = {
+    for (let j = 0; j < randomNumberOfImage; j += 1) {
+      const oneImage = {
         image_url: randomImageUrl,
-        room_id: i
-      }
+        room_id: i,
+      };
       imagesData.push(oneImage);
     }
   }
-}
+};
 
 const seedImages = (data) => {
-  let query = 'INSERT INTO images (image_url, room_id) VALUES(?, ?)';
+  const query = 'INSERT INTO images (image_url, room_id) VALUES(?, ?)';
 
-  for (let i = 0; i< data.length; i++) {
+  for (let i = 0; i < data.length; i += 1) {
     db.query(query, [data[i].image_url, data[i].room_id], (err, result, field) => {
-      if(err) {
-        console.log(err);
+      if (err) {
+        console.log('Error: ', err);
       }
-    })
+    });
   }
 };
 
@@ -69,4 +70,4 @@ seedRooms(roomsData);
 generateImagesData();
 seedImages(imagesData);
 
-console.log('Completed seeding database');
+console.log('message : Completed seeding database');
