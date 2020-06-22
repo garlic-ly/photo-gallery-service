6a63f85397779f860906b11416fb440368807135
tegshee <tegshbayar@gmail.com>
2020-06-21 22:50:12 -0700

    Install styled components and babel plugin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div> This is Photo Gallery from App.jsx </div>
+    <div>
+      <h1> Modern Lounge Ultra clean </h1>
+      <button>4.92 (843) </button>
+      <button>Superhost </button>
+      <a>San Barbara, California, Unites States</a>
+      <button> Share</button>
+      <button> Save</button>
+
+    </div>
   )
 }
 
