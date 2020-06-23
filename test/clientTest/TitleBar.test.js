1f61502b4405c56a4121a4dd499151cd3bd43caa
tegshee <tegshbayar@gmail.com>
2020-06-23 14:43:10 -0700

    Refaactor imagelist and titlebar test for async func calls

--- test/clientTest/TitleBar.test.js
+++ test/clientTest/TitleBar.test.js
@@ -8,31 +8,36 @@ import TitleBar from '../../client/src/components/TitleBar.jsx';
 
 Enzyme.configure({ adapter: new Adapter()});
 
-xdescribe('A suite for TitleBar component', () => {
-  it('should render TitleBar component without throwing an error', () => {
+describe('A suite for TitleBar component', () => {
+  it('should render TitleBar component without throwing an error', done => {
     const wrapper = mount(<App/>);
     expect(wrapper.find(TitleBar).exists()).toEqual(true);
+    done();
   });
 
-  it('should render title for the Room item', () => {
+  it('should render title for the Room item', done => {
     const wrapper = mount(<App/>);
     expect(wrapper.find('h1').exists()).toEqual(true);
+    done();
   });
 
-  it('should render location of city and country', () => {
+  it('should render location of city and country', done => {
     const wrapper = mount(<App/>);
     expect(wrapper.find('a')).toHaveLength(1);
+    done();
   });
 
-  it('should render a button', () => {
+  it('should render a button', done => {
     const wrapper = mount(<App/>);
     expect(wrapper.find('button').exists()).toEqual(true);
+    done()
   });
 
   it('should render 4 buttons for average review, number of reviews, share, save',
-    () => {
+    done => {
     const wrapper = mount(<App/>);
     expect(wrapper.find('button')).toHaveLength(4);
+    done();
   });
 
 })
