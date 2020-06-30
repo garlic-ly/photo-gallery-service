2e5f3d9d71ba5eed6719d9e2dad5961317603225
tegshee <tegshbayar@gmail.com>
2020-06-30 16:56:06 -0700

    Bundle svg and png into bundle.js

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PhotoApp from './components/PhotoApp.jsx';
 
-ReactDOM.render(<PhotoApp/>, document.getElementById('photo-app'));
+ReactDOM.render(<PhotoApp />, document.getElementById('photo-app'));
